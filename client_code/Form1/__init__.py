fd7045dfb8e81738b3fb9c3f616b6bf2d2668d6d
josef.schokobaer@gmail.com <josef.schokobaer@gmail.com>
2024-11-20 11:38:02 +0000

    Created LICENSE.txt; Edited backend and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -6,23 +6,25 @@ from anvil.tables import app_tables
 import anvil.server
 
 
+
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     
     # Any code you write here will run before the form opens.
-    self.gefaengnisse_drop_down.items = anvil.server.call('get_gefaengnisse')
-    self.label_direktor.text = "Direktor TODO" 
-    self.label_freie_zellen.text = "Freie Zellen TODO"
-    self.repeating_zellen.items = [{'zellennummer': 'TODO', 'anzahl_häftlinge': 'TODO'}, 
-                                   {'zellennummer': 'TODO', 'anzahl_häftlinge': 'TODO'}]
+    self.gefaengnisse_drop_down.items = anvil.server.call('get_gefaengnisse', 'Name','GID')
+    self.label_direktor.text = anvil.server.call('get_direktor',"Name",self.gefaengnisse_drop_down.selected_value) 
+    self.label_freie_zellen.text = anvil.server.call('get_zellen',"AnzahlFreieZellen",self.gefaengnisse_drop_down.selected_value)
+    self.repeating_zellen.items = [{'zellennummer': 'TODO', 'anzahl_häftlinge': anvil.server.call('get_zellen',"AnzahlBelegteZellen",self.gefaengnisse_drop_down.selected_value)}, 
+                                   {'zellennummer': 'TODO', 'anzahl_häftlinge': anvil.server.call('get_zellen',"AnzahlBelegteZellen",self.gefaengnisse_drop_down.selected_value)}]
 
   def gefaengnisse_drop_down_change(self, **event_args):
     """This method is called when an item is selected"""
-   
-    pass
-
+    self.label_direktor.text = anvil.server.call('get_direktor',"Name",self.gefaengnisse_drop_down.selected_value) 
+    self.label_freie_zellen.text = anvil.server.call('get_zellen',"AnzahlFreieZellen",self.gefaengnisse_drop_down.selected_value)
+    self.repeating_zellen.items = [{'zellennummer': 'TODO', 'anzahl_häftlinge': anvil.server.call('get_zellen',"AnzahlBelegteZellen",self.gefaengnisse_drop_down.selected_value)}, 
+                                   {'zellennummer': 'TODO', 'anzahl_häftlinge': anvil.server.call('get_zellen',"AnzahlBelegteZellen",self.gefaengnisse_drop_down.selected_value)}]
  
 
 
--- server_code/backend.py
+++ server_code/backend.py
@@ -6,7 +6,27 @@ from anvil.files import data_files
 import anvil.server
 import sqlite3
 
+conn = sqlite3.connect(data_files['prison.db'])
 @anvil.server.callable
-def get_gefaengnisse():
+def get_gefaengnisse(rows, id):
   #return [('TODO 1', 1), ('TODO 2', 2)]
-  return data_files["prison.db"]
+  #conn = sqlite3.connect(data_files['prison.db'])
+  cursor = conn.cursor()
+  res = list(cursor.execute(f"SELECT {rows},{id} FROM Gefängnis"))
+  return res
+@anvil.server.callable
+def get_direktor(DirektorName, GefängnisID):
+  cursor = conn.cursor()
+  res = cursor.execute(f"SELECT Direktor.{DirektorName} from Gefängnis JOIN Verwaltung ON Gefängnis.VID = Verwaltung.VID JOIN Direktor ON Verwaltung.VID = Direktor.VID WHERE Gefängnis.VID = {GefängnisID}")
+  results = res.fetchall()
+  for row in results:
+    direktor_name = str(row[0])
+    return direktor_name
+@anvil.server.callable
+def get_zellen(row,GefängnisID):
+  cursor = conn.cursor()
+  res = cursor.execute(f"SELECT Verwaltung.{row} FROM Gefängnis Join Verwaltung ON Gefängnis.VID = Verwaltung.VID WHERE Gefängnis.VID = {GefängnisID}")
+  results = res.fetchall()
+  for row in results:
+    zelle = str(row[0])
+    return zelle
